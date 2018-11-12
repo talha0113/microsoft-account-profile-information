13d77397af009ccfd9eb915e6825292845c87eb2
Mohammad Talha Nomani <talha0113@hotmail.com>
2018-11-12 19:45:32 +0100

    Update SubscriptionTriggerSubscribeTest.cs

--- NotificationService/MSAccountPushSubscription.Tests/SubscriptionTriggerSubscribeTest.cs
+++ NotificationService/MSAccountPushSubscription.Tests/SubscriptionTriggerSubscribeTest.cs
@@ -41,6 +41,7 @@ namespace MSAccountPushSubscription.Tests
             Environment.SetEnvironmentVariable("CollectionId", TestContext.Properties["CollectionId"].ToString(), EnvironmentVariableTarget.Process);
 
             client = new DocumentClient(new Uri(TestContext.Properties["AccountEndpoint"].ToString()), TestContext.Properties["AccountKey"].ToString());
+            DocumentDBRepository<PushSubscriptionInformation>.Initialize(client);
         }
 
         [TestMethod]
