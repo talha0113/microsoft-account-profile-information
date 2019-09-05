8c2fe18b40f2901a2bf208d7ed7e82bcf9b19e2e
Mohammad Talha Nomani <talha.nomani@outlook.com>
2019-09-05 16:51:06 +0200

    Update environment.prod.ts

--- Application/Configurations/Environments/environment.prod.ts
+++ Application/Configurations/Environments/environment.prod.ts
@@ -5,5 +5,5 @@ export const environment = {
     PWASubscribeUrl: `${functionBaseUrl}/SubscriptionTriggerSubscribe?code=6ZkKhYhLakxFbhu4pSufcaLhmP7CKD1oBrvkHKjFDbyBTNMI/xgynQ==`,
     PWAUnSubscribeUrl: `${functionBaseUrl}/SubscriptionTriggerUnSubscribe?code=KKCazhqqjROnYGkzZ5neeWVptjGIeiKktargagI3bOTel7urtViYww==`,
     PWASubscribeCountUrl: `${functionBaseUrl}/SubscriptionTriggerCount?code=vY/mHKofax5qaD8nmCIqoVKSdJ2YVAIS6Q64irMa5X2pxyivSq31dA==`,
-    PWASignalRConnectionUrl: `${functionBaseUrl}/GetSignalRInformation?code=9aF/gCsxEKrJbgaaxvAMGtrqgdZPCpkVhj92Wr70Un6JQKHrNGNR/w==`
+    PWASignalRConnectionUrl: `${functionBaseUrl}/GetSignalRInformation`
 };
