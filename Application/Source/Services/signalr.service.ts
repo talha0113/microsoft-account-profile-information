169356eeb10bb9f423f88c47920d185bd08ab0c2
Mohammad Talha Nomani <talha.nomani@outlook.com>
2019-09-24 12:00:28 +0200

    @microsoft/signalr

--- Application/Source/Services/signalr.service.ts
+++ Application/Source/Services/signalr.service.ts
@@ -1,7 +1,7 @@
 ﻿import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { Observable, BehaviorSubject } from 'rxjs';
-import { HubConnectionBuilder, HubConnection, HubConnectionState } from "@aspnet/signalr";
+import { HubConnectionBuilder, HubConnection, HubConnectionState } from "@microsoft/signalr";
 
 import { catchError, tap } from 'rxjs/operators';
 import { ErrorManager } from '../Managers/error.manager';
