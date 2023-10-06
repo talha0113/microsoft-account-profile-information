38fe7bae7ac1980eb219b55cba494e60a5fbd3d7
Mohammad Talha Nomani <mohammad.talha@fellowmind.dk>
2023-10-06 11:17:29 +0200

    ci(azure_function): continuous integration and delivery

--- source/notification-service/ms.account.push.subscription.func/Program.cs
+++ source/notification-service/ms.account.push.subscription.func/Program.cs
@@ -1,6 +1,4 @@
-﻿namespace ms.account.push.subscription.func;
-
-using System.Text.Json;
+﻿using System.Text.Json;
 using System.Text.Json.Serialization;
 using Microsoft.Azure.Functions.Worker.Extensions.OpenApi.Extensions;
 using Microsoft.Extensions.DependencyInjection;
@@ -8,32 +6,26 @@ using Microsoft.Extensions.Hosting;
 using ms.account.push.subscription.core;
 using ms.account.push.subscription.infrastructure;
 
-internal class Program
+try
 {
-    internal static void Main()
-    {
-        try
+    var host = new HostBuilder()
+        .ConfigureFunctionsWorkerDefaults()
+        .ConfigureServices((IServiceCollection services) =>
         {
-            var host = new HostBuilder()
-                .ConfigureFunctionsWorkerDefaults()
-                .ConfigureServices((IServiceCollection services) =>
-                {
-                    _ = services.AddCore();
-                    _ = services.AddInfrastructure();
-                    _ = services.Configure<JsonSerializerOptions>(options =>
-                    {
-                        options.PropertyNamingPolicy = JsonNamingPolicy.CamelCase;
-                        options.Converters.Add(new JsonStringEnumConverter(namingPolicy: JsonNamingPolicy.CamelCase, allowIntegerValues: false));
-                    });
-                })
-                .ConfigureOpenApi()
-                .Build();
-            host.Run();
-        }
-        catch (Exception ex) 
-        {
-            Console.WriteLine(ex.Message);
-            Console.WriteLine(ex.StackTrace);
-        }
-    }
+            _ = services.AddCore();
+            _ = services.AddInfrastructure();
+            _ = services.Configure<JsonSerializerOptions>(options =>
+            {
+                options.PropertyNamingPolicy = JsonNamingPolicy.CamelCase;
+                options.Converters.Add(new JsonStringEnumConverter(namingPolicy: JsonNamingPolicy.CamelCase, allowIntegerValues: false));
+            });
+        })
+        .ConfigureOpenApi()
+        .Build();
+    await host.RunAsync();
+}
+catch (Exception ex) 
+{
+    Console.WriteLine(ex.Message);
+    Console.WriteLine(ex.StackTrace);
 }
