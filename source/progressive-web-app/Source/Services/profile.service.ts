36e0c1d33649bb3f5dafba0fdac354d4ef5985f5
Mohammad Talha Nomani <mohammad.talha@fellowmind.dk>
2023-10-10 17:59:48 +0200

    build(pwa): update to version 15

--- source/progressive-web-app/Source/Services/profile.service.ts
+++ source/progressive-web-app/Source/Services/profile.service.ts
@@ -1,11 +1,11 @@
-﻿import { Injectable } from '@angular/core';
-import { HttpClient } from '@angular/common/http';
+﻿import { HttpClient } from '@angular/common/http';
 import { Observable, forkJoin, of } from 'rxjs';
 
 import { Profile } from '../Models/profile.model';
 import { map, catchError, tap, delay } from 'rxjs/operators';
 import { GraphConstant } from '../Constants/graph.constant';
 import { ProfileStore } from '../Stores/profile.store';
+import { Injectable } from '@angular/core';
 
 @Injectable()
 export class ProfileService {
@@ -15,13 +15,13 @@ export class ProfileService {
     get information(): Observable<Profile> {
         let basicInformation: Observable<Profile> = this.httpClient.get<Profile>(GraphConstant.profileMetaDataUrl).pipe(catchError((error) => { return of(null); }));
         let profilePicture: Observable<Blob> = this.httpClient.get(GraphConstant.profilePictureUrl, { responseType: "blob" }).pipe(catchError((error) => { return of(null); }));
-        return forkJoin<Profile, Blob>(basicInformation, profilePicture).pipe(map((values: [Profile, Blob]) => {
-            if (values["0"] == null && values["1"] == null) {
+        return forkJoin({ basicInformation, profilePicture }).pipe(map((values: { basicInformation: Profile, profilePicture: Blob }) => {
+            if (values.basicInformation == null && values.profilePicture == null) {
                 return null;
             }
             else {
-                let profileInformation: Profile = values["0"];
-                profileInformation.imageUrl = values["1"];
+                let profileInformation: Profile = values.basicInformation;
+                profileInformation.imageUrl = values.profilePicture;
                 return profileInformation;
             }
         })).pipe(tap((value: Profile) => {
