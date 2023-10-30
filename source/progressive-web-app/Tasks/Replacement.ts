94da40c39ed3c0d84ec76501ed312f7a68858ac1
Mohammad Talha Nomani <mohammad.talha@fellowmind.dk>
2023-10-30 14:06:27 +0100

    build(pwa): gzipper as a compression library

--- source/progressive-web-app/Tasks/Replacement.ts
+++
@@ -1,5 +0,0 @@
-import { readFileSync, writeFileSync } from "fs";
-let serviceWorkerFile = readFileSync('./Distributions/ngsw-worker.js').toString();
-let serviceWorkerModificationFile = readFileSync('./Tasks/ServiceWorkerModification.txt').toString();
-let newServiceWorkerFile = serviceWorkerFile.replace("this.scope.addEventListener('notificationclick', (event) => this.onClick(event));", serviceWorkerModificationFile);
-writeFileSync('./Distributions/ngsw-worker.js', newServiceWorkerFile,)
