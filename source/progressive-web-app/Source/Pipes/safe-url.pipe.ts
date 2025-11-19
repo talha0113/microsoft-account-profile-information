fba33fbee67477d04df430d2f0a7113007e351af
Mohammad Talha Nomani <mohammad.talha@fellowmind.dk>
2025-11-19 14:25:11 +0100

    feat(signals): switch to signals

--- source/progressive-web-app/Source/Pipes/safe-url.pipe.ts
+++ source/progressive-web-app/Source/Pipes/safe-url.pipe.ts
@@ -1,7 +1,7 @@
 ﻿import { Pipe, PipeTransform, inject } from '@angular/core';
 import { DomSanitizer, SafeResourceUrl } from '@angular/platform-browser';
 
-@Pipe({ name: 'safeUrl' })
+@Pipe({ name: 'safeUrl', standalone: true })
 export class SafeUrlPipe implements PipeTransform {
   private readonly domSanitizer = inject(DomSanitizer);
 
