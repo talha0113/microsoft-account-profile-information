da9cd68567c8fd5530956cb9658f8b15c73741c7
Mohammad Talha Nomani <mohammad.talha@fellowmind.dk>
2025-11-28 12:57:54 +0100

    chore(angular): lint fix

--- source/progressive-web-app/Configurations/Environments/environment.prod.ts
+++ source/progressive-web-app/Configurations/Environments/environment.prod.ts
@@ -1,11 +1,12 @@
-const FUNCTION_CODE = "#{FunctionCode}#";
-const FUNCTION_BASE_URL = "https://func-msaccprofinfo-dev-001.azurewebsites.net/api";
+const FUNCTION_CODE = '#{FunctionCode}#';
+const FUNCTION_BASE_URL =
+  'https://func-msaccprofinfo-dev-001.azurewebsites.net/api';
 
 export const environment = {
-    production: true,
-    PWASubscribeUrl: `${FUNCTION_BASE_URL}/SubscriptionTriggerSubscribe?code=${FUNCTION_CODE}`,
-    PWAUnSubscribeUrl: `${FUNCTION_BASE_URL}/SubscriptionTriggerUnSubscribe?code=${FUNCTION_CODE}`,
-    PWASubscribeCountUrl: `${FUNCTION_BASE_URL}/SubscriptionTriggerCount?code=${FUNCTION_CODE}`,
-    PWASignalRConnectionUrl: `${FUNCTION_BASE_URL}/SubscriptionTriggerSignalR?code=${FUNCTION_CODE}`,
-    PWAUpdateLanguageUrl: `${FUNCTION_BASE_URL}/SubscriptionTriggerUpdateLanguage?code=${FUNCTION_CODE}`
+  production: true,
+  PWASubscribeUrl: `${FUNCTION_BASE_URL}/SubscriptionTriggerSubscribe?code=${FUNCTION_CODE}`,
+  PWAUnSubscribeUrl: `${FUNCTION_BASE_URL}/SubscriptionTriggerUnSubscribe?code=${FUNCTION_CODE}`,
+  PWASubscribeCountUrl: `${FUNCTION_BASE_URL}/SubscriptionTriggerCount?code=${FUNCTION_CODE}`,
+  PWASignalRConnectionUrl: `${FUNCTION_BASE_URL}/SubscriptionTriggerSignalR?code=${FUNCTION_CODE}`,
+  PWAUpdateLanguageUrl: `${FUNCTION_BASE_URL}/SubscriptionTriggerUpdateLanguage?code=${FUNCTION_CODE}`,
 };
