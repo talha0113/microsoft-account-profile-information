7bfc42adb0ba310180fbc16be7eab49a00d5e2d8
Mohammad Talha Nomani <mohammad.talha@fellowmind.dk>
2026-01-09 16:27:20 +0100

    chore(notification): local settings

--- source/progressive-web-app/Source/Configurations/authentication.configuration.ts
+++ source/progressive-web-app/Source/Configurations/authentication.configuration.ts
@@ -2,6 +2,6 @@
   public static readonly applicationId: string =
     'e5a6e3e8-7a9e-47d7-b0bd-0d78ebe60257';
   public static readonly authority: string =
-    'https://login.microsoftonline.com/common';
+    'https://login.microsoftonline.com/7e593fb7-6cc2-4623-9e6b-8564cd5ca40d';
   public static readonly scopes: string[] = ['OpenId', 'User.Read', 'profile'];
 }
