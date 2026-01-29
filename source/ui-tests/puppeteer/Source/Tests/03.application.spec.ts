558480990edb5500ab66ea5861232725cc1d721c
Mohammad Talha Nomani <talha.nomani@outlook.com>
2026-01-29 10:48:30 +0100

    fix(pwa): logout changes

--- source/ui-tests/puppeteer/Source/Tests/03.application.spec.ts
+++ source/ui-tests/puppeteer/Source/Tests/03.application.spec.ts
@@ -56,7 +56,7 @@ describe('Microsoft Account Information Application', () => {
         await page.click('navigation > div > nav > span:nth-child(3) > a');
         await page.waitForSelector('div > div > logout > div > button');
         await page.click('div > div > logout > div > button');
-        expect(page.url()).toContain("/login");
+        expect(page.url()).toContain("/logoutsession");
     }, timeout);
 
     afterAll(async () => {
